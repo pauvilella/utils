cb393c778d2600a4be9e7f8b052280165d7518ba
Pau Vilella <pau.vilella@chartboost.com>
2023-12-04 10:56:13 +0100

    Update vscode settings and script

--- bin/update_vscode_settings.sh
+++ bin/update_vscode_settings.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-rm -rf .vscode
-cp -r ~/git/pauvilella/utils/.vscode .vscode
+rm .vscode/settings.json .vscode/extensions.json
+cp ~/git/pauvilella/utils/.vscode/settings.json .vscode/settings.json
+cp ~/git/pauvilella/utils/.vscode/extensions.json .vscode/extensions.json
